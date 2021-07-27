0bef72a1f31373694f98dcc4947588c0d68f9510
zjfcool <2568696196@qq.com>
2021-07-27 15:02:33 +0800

    init

---
+++ config/defaultSettings.js
@@ -0,0 +1,15 @@
+const Settings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#000',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  colorWeak: true,
+  title: 'Ant Design Pro',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+export default Settings;
